3e19a8c25759f75e84faaeede0c22ea94a4fd7d9
Bruno Ramalho <bramalho@divante.pl>
2020-07-20 15:58:30 +0200

    bashboard

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/", name="admin")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Super Duper Eureka');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        return [
+            MenuItem::linktoDashboard('Dashboard', 'fa fa-home')
+        ];
+    }
+}
